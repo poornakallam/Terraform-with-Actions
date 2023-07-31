81df875e4fef9a717b5ee395a815e802c8f546f6
poorna kallam <poorna.kallam2gmail.com>
2023-07-31 08:39:08 +0530

    test

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,3 +1,9 @@
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "./pp.py"
+output_path = "./pp.zip"
+}
+
 resource "aws_lambda_function" "test_lambda" {
   filename      = "./pp.zip"
   function_name = var.function_name
